f63218c036d6b4f6cd96bd06c53dd6a65d8fe2bd
Matthew Magee <matthewmagee05@gmail.com>
2019-04-07 11:52:58 -0500

    updated project

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,7 +6,7 @@ import tw from 'tailwind.macro'
 const Wrapper = styled.a`
   width: 100%;
   ${tw`shadow-lg relative no-underline rounded-lg px-8 py-8 md:py-24 text-white`};
-  background: ${props => props.bg};
+  background-image: url('${props => props.bg}');
   transition: transform 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275);
   &:hover {
     transform: translateY(-5px);
@@ -15,18 +15,19 @@ const Wrapper = styled.a`
 
 const Text = styled.div`
   ${tw`opacity-75 font-sans text-sm md:text-base`};
-  text-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
+  text-shadow: 0 2px 10px rgba(0, 0, 0, 0.9);
 `
 
 const Title = styled.div`
   ${tw`text-white uppercase text-2xl md:text-3xl xl:text-4xl tracking-wide font-sans pt-8`};
-  text-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
+  text-shadow: 0 2px 10px rgba(0, 0, 0, 0.9);
+  background: 
 `
 
 const ProjectCard = ({ title, link, children, bg }) => (
   <Wrapper href={link} target="_blank" rel="noopener noreferrer" bg={bg}>
-    <Text>{children}</Text>
     <Title>{title}</Title>
+    <Text>{children}</Text>
   </Wrapper>
 )
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -18,6 +18,10 @@ import About from '../views/About'
 import Contact from '../views/Contact'
 
 import avatar from '../images/avatar.jpg'
+import brewtodo from '../images/brewtodo.jpeg'
+import rest from '../images/rest.gif'
+import pokemon from '../images/optimized-pokemon.jpg'
+import drum from '../images/drum.jpg'
 
 const ProjectsWrapper = styled.div`
   ${tw`flex flex-wrap justify-between mt-8`};
@@ -63,40 +67,40 @@ const Index = () => (
     <Parallax pages={5}>
       <Hero offset={0}>
         <BigTitle>
-          Hello, <br /> I'm John Doe.
+          Hello, <br /> I'm Matthew Magee.
         </BigTitle>
-        <Subtitle>I'm creating noice web experiences for the next generation of consumer-facing companies.</Subtitle>
+        <Subtitle>I'm a full stack developer creating next generation web experiences for consumer-facing companies.</Subtitle>
       </Hero>
       <Projects offset={1}>
         <Title>Projects</Title>
         <ProjectsWrapper>
           <ProjectCard
-            title="Freiheit"
-            link="https://www.behance.net/gallery/58937147/Freiheit"
-            bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
+            title="BrewTodo"
+            link="https://github.com/matthewmagee05/brewtodo"
+            bg={brewtodo}
           >
-            This project is my entry to Adobe's #ChallengeYourPerspective contest.
+            Keep track of your brewery escapades.
           </ProjectCard>
           <ProjectCard
-            title="Harry Potter"
-            link="https://www.behance.net/gallery/52915793/Harry-Potter"
-            bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)"
+            title="Restaurant Finder"
+            link="https://github.com/matthewmagee05/matthew-magee-project1"
+            bg={rest}
           >
-            I entered the DOCMA 2017 award with this Harry Potter inspired image.
+            ASP.NET MVC Restaurant Application
           </ProjectCard>
           <ProjectCard
-            title="Tomb Raider"
-            link="https://www.behance.net/gallery/43907099/Tomb-Raider"
-            bg="linear-gradient(to right, #009245 0%, #FCEE21 100%)"
+            title="React Native App"
+            link="https://github.com/matthewmagee05/PokeSearch"
+            bg={pokemon}
           >
-            Recreation of a Tomb Raider Wallpaper (Fan Art)
+            Gotta Catch em all!
           </ProjectCard>
           <ProjectCard
-            title="Eagle"
-            link="https://www.behance.net/gallery/38068151/Eagle"
-            bg="linear-gradient(to right, #D585FF 0%, #00FFEE 100%)"
+            title="JavaScript Drum Kit"
+            link="https://github.com/matthewmagee05/javascript-drum-kit"
+            bg={drum}
           >
-            A fantasy image manipulation relocating the habitat of wild animals.
+            Fun for all ages.
           </ProjectCard>
         </ProjectsWrapper>
       </Projects>
@@ -105,31 +109,23 @@ const Index = () => (
         <AboutHero>
           <Avatar src={avatar} alt="John Doe" />
           <AboutSub>
-            The English language can not fully capture the depth and complexity of my thoughts. So I'm incorporating
-            Emoji into my speech to better express myself. Winky face.
+          If at first you don’t succeed; call it version 1.0
           </AboutSub>
         </AboutHero>
         <AboutDesc>
-          You know the way you feel when you see a picture of two otters holding hands? That's how you're gonna feel
-          every day. My mother cried the day I was born because she knew she’d never be prettier than me. You should
-          make me your campaign manager. I was born for politics. I have great hair and I love lying. Captain? The kids
-          want to know where Paulie the Pigeon is. I told them he got sucked up into an airplane engine, is that all
-          right?
+        I am a .NET web developer focusing on full stack development with C#. I have created web clients with JavaScript, HTML5, CSS3, JQuery, ASP.NET MVC 5 and Angular. I have implemented back-end technologies including ADO.NET Entity Framework for data access, SQL Server and NoSQL databases like MongoDB. In addition, I have worked in diverse team environments following Agile/Scrum methodologies. 
         </AboutDesc>
       </About>
       <Contact offset={4}>
         <Inner>
           <Title>Get in touch</Title>
           <ContactText>
-            Say <a href="mailto:plizNoSp4m@domain.tld">Hi</a> or find me on other platforms:{' '}
-            <a href="https://dribbble.com/LekoArts">Dribbble</a> &{' '}
-            <a href="https://www.instagram.com/lekoarts.de/">Instagram</a>
+            Say <a href="mailto:matthewmagee05@gmail.com">Hi</a> or find me on other platforms:{' '}
+            <a href="https://www.linkedin.com/in/mmagee05/">LinkedIn</a> &{' '}
+            <a href="https://twitter.com/matthewmagee05">Twitter</a>
           </ContactText>
         </Inner>
         <Footer>
-          &copy; 2019 by Gatsby Starter Portfolio Cara.{' '}
-          <a href="https://github.com/LekoArts/gatsby-starter-portfolio-cara">Github Repository</a>. Made by{' '}
-          <a href="https://www.lekoarts.de">LekoArts</a>.
         </Footer>
       </Contact>
     </Parallax>
